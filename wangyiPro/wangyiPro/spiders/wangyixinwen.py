c1612c69bf2b9f03ebd64b52fecaa7702a97e5a9
LiJinXI1007 <1328150467@qq.com>
2020-05-04 08:33:52 +0800

    更换api爬取

--- wangyiPro/wangyiPro/spiders/wangyixinwen.py
+++ wangyiPro/wangyiPro/spiders/wangyixinwen.py
@@ -1,13 +1,15 @@
 import requests
 import json
-url = 'http://c.m.163.com/nc/article/headline/T1348647853363/0-100.html'
+url='http://c.3g.163.com/nc/article/list/T1348648517839/0-20.html'
+#url = 'http://c.m.163.com/nc/article/headline/T1348647853363/0-100.html'
 header = {
         'User-Agent': 'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1)',
         'Connection': 'keep - alive',
         }
 wbdata = requests.get(url,headers=header).text
 data = json.loads(wbdata)
-news = data['T1348647853363']
+news=data['T1348648517839']
+#news = data['T1348647853363']
 for item in news:
     digest = item['digest']
     mtime = item['mtime']
