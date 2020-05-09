ba9cec4050b591a88321d117f3ae05f764f17cc4
lijinxi <1328150467@qq.com>
2020-05-09 11:29:17 +0800

    加入scrapy框架

--- wangyiPro/wangyiPro/spiders/wangyi_api.py
+++ wangyiPro/wangyiPro/spiders/wangyi_api.py
@@ -1,5 +1,4 @@
 import scrapy
-import re
 import json
 from selenium import webdriver
 from wangyiPro.settings import WANGYI_API
@@ -7,9 +6,12 @@ from wangyiPro.settings import WANGYI_API
 class WangYiSpider(scrapy.Spider):
     name="wangyi_api"
     start_urls = ['https://news.163.com/']
-    bro = webdriver.Chrome(executable_path='chromedriver.exe')
+def __init__(self):
+     self.bro = webdriver.Chrome(executable_path='chromedriver.exe')
+
 def start_requests(self):
     yield scrapy.Request(url=self.start_urls[0],dont_filter=True,cookies=None)
+
 def parse(self, response):
     header = {
         'User-Agent': 'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1)',
@@ -18,3 +20,24 @@ def parse(self, response):
     wbdata=response.get(WANGYI_API,headers=header).text
     data = json.loads(wbdata)
     news = data['T1348648517839']
+    for item in news:
+        digest = item['digest']
+        mtime = item['mtime']
+        title = item['title']
+        source = item['source']
+        try:
+            url = item['url']
+        except:
+            url = ''
+        newes_data = {
+            'title': title,
+            '内容': digest,
+            '时间': mtime,
+            '来源': source,
+            '链接': url,
+        }
+        print(newes_data)
+def closed(self, spider):
+            # 实现父类方法，爬虫结束时调用
+        print("爬虫结束")
+        self.bro.quit()
